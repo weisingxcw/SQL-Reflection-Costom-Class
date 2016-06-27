82f252d9de423957a965bcc0e074f5f94226e809
weisingxcw <weisingxcw@outlook.com>
2016-06-27 12:36:22 +0800

    Edit 2016.6.27

--- Index.aspx.cs
+++ Index.aspx.cs
@@ -14,22 +14,6 @@ namespace VANCHEER.Web
 {
     public partial class Index : System.Web.UI.Page
     {
-        BasicInfoModel BasicInfoMethod = new BasicInfoModel();
-        protected BasicInfoModel.BasicInfoValue GetBasicInfo = new BasicInfoModel.BasicInfoValue();
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            BasicInfoMethod.Load();
-            GetIndexData();
-        }
-
-        protected void GetIndexData()
-        {
-            StringBuilder sqlGetData = new StringBuilder().AppendFormat(
-                @"With Recommend SELECT Top 1 ID,Title FROM V_C_RecommendContent WHERE IsDel=0 and FLowstate=99 Order by Orders");
-            var reuslt = sqlReflectClass<IndexConfig>(SQLHelper.ConnectionStringLocalTransaction, sqlGetData.ToString());
-        }
-
         /// 获取SQL反射泛型集合
         /// </summary>
         /// <param name="connStr">数据库连接字符串</param>
@@ -59,32 +43,6 @@ namespace VANCHEER.Web
                     }
                     sqlConnection.Close();
                     return list;
-                }
-            }
-        }
-
-        /// 获取SQL反射泛型集合
-        /// </summary>
-        /// <param name="connStr">数据库连接字符串</param>
-        /// <param name="sqlStr">要查询的T-SQL</param>
-        /// <returns></returns>
-        protected T sqlReflectClassSingle<T>(string connStr, string sqlStr) where T : class,new()
-        {
-            using (SqlConnection sqlConnection = new SqlConnection(connStr))
-            {
-                sqlConnection.Open();
-                using (SqlCommand cmd = new SqlCommand(sqlStr, sqlConnection))
-                {
-                    SqlDataReader dataReader = cmd.ExecuteReader();
-                    T CostomClass = new T();
-                    PropertyInfo[] ClassProperty = CostomClass.GetType().GetProperties();
-                    if (!dataReader.Read()) { return CostomClass; }
-                    foreach (var Property in ClassProperty)
-                    {
-                        Property.SetValue(CostomClass, dataReader[Property.Name], null);
-                    }
-                    sqlConnection.Close();
-                    return CostomClass;
                 }
             }
         }
